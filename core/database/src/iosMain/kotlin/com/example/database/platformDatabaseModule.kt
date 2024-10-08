ef5e865817c941633823e47942d3bcd98b5c3964
mac <igorv8836@mail.ru>
2024-10-08 13:32:52 +0300

    fix(room): the project is going to be built for ios

--- core/database/src/iosMain/kotlin/com/example/database/platformDatabaseModule.kt
+++ core/database/src/iosMain/kotlin/com/example/database/platformDatabaseModule.kt
@@ -3,7 +3,12 @@ package com.example.database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.database.database.AppDatabase
+import com.example.database.database.instantiateImpl
+import kotlinx.cinterop.ExperimentalForeignApi
 import org.koin.dsl.module
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSUserDomainMask
 
 actual fun platformDatabaseBuilderModule() = module {
     single(createdAtStart = true) { provideDatabase() }
@@ -11,11 +16,13 @@ actual fun platformDatabaseBuilderModule() = module {
 
 private fun provideDatabase(): RoomDatabase.Builder<AppDatabase> {
     val dbFilePath = documentDirectory() + "/app_database.db"
-    return Room.databaseBuilder<AppDatabase>(
-        name = dbFilePath
+    return Room.databaseBuilder(
+        name = dbFilePath,
+        factory = { AppDatabase::class.instantiateImpl() }
     )
 }
 
+@OptIn(ExperimentalForeignApi::class)
 private fun documentDirectory(): String {
     val documentDirectory = NSFileManager.defaultManager.URLForDirectory(
         directory = NSDocumentDirectory,
--- core/datastore/src/iosMain/kotlin/com/example/datastore/createDataStore.ios.kt
+++ core/datastore/src/iosMain/kotlin/com/example/datastore/createDataStore.ios.kt
@@ -2,10 +2,13 @@ package com.example.datastore
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import kotlinx.cinterop.ExperimentalForeignApi
 import platform.Foundation.NSDocumentDirectory
 import platform.Foundation.NSFileManager
+import platform.Foundation.NSURL
 import platform.Foundation.NSUserDomainMask
 
+@OptIn(ExperimentalForeignApi::class)
 fun createDataStore(): DataStore<Preferences> = createDataStore(
     producePath = {
         val documentDirectory: NSURL? = NSFileManager.defaultManager.URLForDirectory(
